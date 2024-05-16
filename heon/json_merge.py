8cadba04fede22abf35df5049738a67e57cae839
jaybee-heon <changheon.whale@gmail.com>
2024-05-16 10:51:08 +0900

    relevant class

--- heon/json_merge.py
+++ heon/json_merge.py
@@ -2,10 +2,10 @@ import json
 
 # 읽을 JSON 파일들의 경로
 file_paths = [
-    './all_class_mutation/Chart-1_fdr_0.json',
-    './all_class_mutation/Chart-1_fdr_1.json',
-    './all_class_mutation/Chart-1_fdr_2.json',
-    './all_class_mutation/Chart-1_fdr_3.json'
+    './relevant_class_mutation/Chart-1_rel_class_fdr_0.json',
+    './relevant_class_mutation/Chart-1_rel_class_fdr_1.json',
+    './relevant_class_mutation/Chart-1_rel_class_fdr_2.json',
+    './relevant_class_mutation/Chart-1_rel_class_fdr_3.json'
 ]
 merged_data = []
 
@@ -21,7 +21,7 @@ for file_path in file_paths:
                 merged_data["Chart_1"][key] = value
 
 # 병합된 데이터를 새로운 JSON 파일로 저장
-with open('./Chart-1_fdr.json', 'w', encoding='utf-8') as merged_file:
+with open('./Chart-1_rel_class_fdr.json', 'w', encoding='utf-8') as merged_file:
     json.dump(merged_data, merged_file, ensure_ascii=False, indent=4)
 
 print("JSON 파일이 성공적으로 병합되었습니다.")
