1499685ef38e81237b58ef6d79f3929b97ca62c9
cjs0410 <jinsuchoi@kaist.ac.kr>
2024-05-29 17:36:00 +0900

    Modify pymoo

--- run_ga.py
+++ run_ga.py
@@ -6,8 +6,10 @@ from data_preprocess import json2PandasDf
 from pymoo.operators.sampling.rnd import BinaryRandomSampling
 from pymoo.operators.crossover.sbx import SBX
 from pymoo.operators.mutation.pm import PM
+from pymoo.operators.mutation.bitflip import BitflipMutation
+from pymoo.operators.mutation.am import AdequacyMutation
 from pymoo.visualization.scatter import Scatter
-
+from sklearn.preprocessing import MinMaxScaler
 # test_cases = json2PandasDf("data/tet.json")
 
 # Define the test case prioritization problem
@@ -29,18 +31,18 @@ class TestCasePrioritizationProblem(ElementwiseProblem):
         # Objectives: Minimize execution time and maximize fault detection
         out["F"] = np.array([total_execution_time, -total_fault_detection])
 
-
 # Initialize the problem
 def run_nsga(test_cases):
     problem = TestCasePrioritizationProblem(test_cases)
-   
+    
     # Set the algorithm configuration
     algorithm = NSGA2(
         pop_size=100,
         n_offsprings=10,
         sampling=BinaryRandomSampling(), ## 이 후에 다른 sampling 전략(coverage 높은 순서대로)을 적용해야 함.
         crossover=SBX(prob=0.9, eta=15),
-        mutation=PM(eta=20)
+        # mutation=PM(eta=20)
+        mutation=BitflipMutation(prob=0.5, prob_var=1/len(test_cases))
     )
 
     # Run the optimization
@@ -54,6 +56,44 @@ def run_nsga(test_cases):
     for solution in res.X:
         print(f"Test Cases Selected: {solution.astype(int)}")
         print(f"Execution Time: {np.sum(test_cases[solution.astype(bool), 0])}, Fault Detection: {np.sum(test_cases[solution.astype(bool), 1])}")
+
+
+# Initialize the problem
+def run_nsga_with_adequecy(test_cases, adequacy_scores):
+    problem = TestCasePrioritizationProblem(test_cases)
+    
+    # Set the algorithm configuration
+    algorithm = NSGA2(
+        pop_size=100,
+        n_offsprings=10,
+        sampling=BinaryRandomSampling(), ## 이 후에 다른 sampling 전략(coverage 높은 순서대로)을 적용해야 함.
+        crossover=SBX(prob=0.9, eta=15),
+        # mutation=PM(eta=20)
+        mutation=AdequacyMutation(prob=1.0, prob_var=0.3, adeq_scores=np.transpose(adequacy_scores))
+    )
+
+    # Run the optimization
+    res = minimize(problem,
+                algorithm,
+                ("n_gen", 40),
+                verbose=True)
+
+    # Output the results
+    print("Optimal Test Case Selections:")
+    for solution in res.X:
+        print(f"Test Cases Selected: {solution.astype(int)}")
+        print(f"Execution Time: {np.sum(test_cases[solution.astype(bool), 0])}, Fault Detection: {np.sum(test_cases[solution.astype(bool), 1])}")
+
+def get_adequacy_scores(data):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    norm = (data - min_val) / (max_val - min_val)
+    r_norm = 1 - norm
+    selection_prob = norm / np.sum(norm)
+    removal_prob = r_norm / np.sum(r_norm)
+    adequacy_scores = np.column_stack((selection_prob, removal_prob))
+
+    return adequacy_scores
 
 if __name__ == "__main__":
     # Sample data: [execution time, fault detection rate]
@@ -77,4 +117,7 @@ if __name__ == "__main__":
     # Combine into a single array
     test_cases = np.column_stack((execution_times, fault_detections))
 
-    run_nsga(test_cases)    
+    # Calculate adequacy score
+    adequacy_scores = get_adequacy_scores(fault_detections)
+
+    run_nsga(test_cases, adequacy_scores)
