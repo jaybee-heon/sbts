6de1fee11e31686743c505303a6b1ea1666902c1
useruseruse <eliza1102@kaist.ac.kr>
2024-06-04 20:44:10 +0900

    fixed version of experiment result

--- experiment.py
+++ experiment.py
@@ -46,14 +46,14 @@ def get_adequacy_scores(data, scaling_method='min_max'):
     adequacy_scores = np.column_stack((avg_selection_prob, avg_removal_prob))
     return adequacy_scores
 
-def run_bitflip(data, metric, execution_times, project_name, output_path):
+def run_bitflip(data, metric, execution_times):
     test_cases = np.column_stack((execution_times, metric))
     bitflip_res, exec_times, fault_detections = run_nsga(test_cases, verbose=False)
     fault_detections_rate = fault_detections / np.max(fault_detections)
     ets, fds = [exec_times], [fault_detections_rate]
     return ets, fds, bitflip_res
 
-def run_adequacy(data, adequacy_metric, execution_times, project_name, output_path, mutation):
+def run_adequacy(data, adequacy_metric, execution_times):
     adequacy_scores = get_adequacy_scores(adequacy_metric)
     test_cases = np.column_stack((execution_times, data['coverage']))
     adeq_res, exec_times, fault_detections = run_nsga_with_adequecy(test_cases, adequacy_scores, verbose=False)
@@ -72,7 +72,6 @@ def collect_result(fitness='cov', mutations=['fdr', 'cov']):
             with open(os.path.join(datadir, pkl_file), 'rb') as f: # load pickle file for projects from merged_data
                 data = pickle.load(f)
             project_name = os.path.splitext(pkl_file)[0]
-            output_path = os.path.join("./data/experiment_result/", f"f_{fitness}_m_{'_'.join(mutations)}")
             print(f'\nProject: {project_name}--------------------')
 
             # Metric to use for fitness
@@ -85,8 +84,8 @@ def collect_result(fitness='cov', mutations=['fdr', 'cov']):
 
             execution_times = data['tet']
 
-            # Run bitflip approach
-            bitflip_ets, bitflip_fds, bitflip_res = run_bitflip(data, metric, execution_times, project_name, output_path)
+            # Run bitflip approach 
+            bitflip_ets, bitflip_fds, bitflip_res = run_bitflip(data, metric, execution_times)
             if project_name not in all_ets_dict:
                 all_ets_dict[project_name] = {}
                 all_fds_dict[project_name] = {}
@@ -94,9 +93,11 @@ def collect_result(fitness='cov', mutations=['fdr', 'cov']):
             all_ets_dict[project_name]['bitflip'] = bitflip_ets
             all_fds_dict[project_name]['bitflip'] = bitflip_fds
             all_res_dict[project_name]['bitflip'] = bitflip_res
-            
+            print("before running adequacy", data.shape)  
+
             # Run adequacy approach
             for mutation in mutations:
+                output_path = os.path.join("./data/experiment_result/", f"f_{fitness}_m_{mutation}")
                 # Metric to use for adequacy score
                 match mutation:
                     case 'fdr':
@@ -109,27 +110,28 @@ def collect_result(fitness='cov', mutations=['fdr', 'cov']):
                         adeq_metric = data['latest_fixed']
                     case 'all':
                         adeq_metric = data.loc[:, ['fdr', 'fixed_line_cov', 'coverage', 'latest_fixed']]
-                adequacy_ets, adequacy_fds, adeq_res = run_adequacy(data, adeq_metric, execution_times, project_name, output_path, mutation)
+                adequacy_ets, adequacy_fds, adeq_res = run_adequacy(data, adeq_metric, execution_times)
+                print("after running adequacy", data.shape, adeq_res)  
                 all_ets_dict[project_name][mutation] = adequacy_ets
                 all_fds_dict[project_name][mutation] = adequacy_fds
                 all_res_dict[project_name][mutation] = adeq_res
 
-            # Save the results
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            with open(os.path.join(output_path, f"{project_name}_bitflip.pkl"), 'wb') as f:
-                pickle.dump(bitflip_res, f)
-            for mutation in mutations:
-                with open(os.path.join(output_path, f"{project_name}_{mutation}_adeq.pkl"), 'wb') as f:
+                # Save the results of each mutation
+                if not os.path.exists(output_path):
+                    os.makedirs(output_path)
+                with open(os.path.join(output_path, f"{project_name}_adeq.pkl"), 'wb') as f:
                     pickle.dump(all_res_dict[project_name][mutation], f)
 
+                with open(os.path.join(output_path, f"{project_name}_bitflip.pkl"), 'wb') as f:
+                    pickle.dump(bitflip_res, f)
+                
     return all_ets_dict, all_fds_dict
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Optimize project with LLM")
     parser.add_argument("-f", "--fitness", default='cov', help="List of fitness methods to use")
-    parser.add_argument("-m", "--mutations", nargs='+', default=['fdr', 'flc', 'cov', 'latest', 'all'], help="List of mutation methods to use")
+    parser.add_argument("-m", "--mutations", nargs='+', default=['fdr', 'flc', 'cov', 'latest'], help="List of mutation methods to use")
     args = parser.parse_args()
 
     all_ets_dict, all_fds_dict = collect_result(args.fitness, args.mutations)
